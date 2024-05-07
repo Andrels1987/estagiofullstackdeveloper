bc4ca5017434f19f70b3ce2e7bd85936b2ee28f7
Andrels <alspersonalmail@gmail.com>
2024-05-07 00:14:52 -0300

    last commit

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+let PORT = 3500;
 
 app.use(express.static(path.join(__dirname,'public')))
 
@@ -8,7 +9,6 @@ app.use("/home", (req, res) =>{
     res.sendFile(path.join(__dirname,'../frontend/views/index.html'));
 });
 
-let PORT = 3500;
 
 //starting the server
 app.listen(PORT, () => {
